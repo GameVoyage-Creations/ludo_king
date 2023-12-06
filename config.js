e90be3e4464454da2df6377d3ed7deb5f4d59de8
tamrat-ake <tamratfekadu123@gmail.com>
2023-12-06 12:44:08 +0300

    creating events for both bottons

--- config.js
+++ config.js
@@ -10,7 +10,31 @@ function init() {
     .classList.remove("hidden");
 }
 init();
-
+// creating functiion that generate rondom number
 const rondomDice = function () {
   return Math.floor(Math.random() * 6 + 1);
 };
+
+// creating buttons for both buttons
+
+btnEl0.addEventListener("click", function () {
+  // generating random dice
+  const dice = rondomDice();
+  // cheaking if the dice less that 6
+  if (dice < 6) switchPlayer();
+  else {
+    // displying the dice
+    btnEl0.src = `./Images/Dice-images/dice-${dice}.png`;
+  }
+});
+btnEl1.addEventListener("click", function () {
+  // generating random dice
+  const dice = rondomDice();
+  console.log(dice);
+  // cheaking if the dice less that 6
+  if (dice < 6) switchPlayer();
+  else {
+    // displying the dice
+    btnEl1.src = `./Images/Dice-images/dice-${dice}.png`;
+  }
+});
