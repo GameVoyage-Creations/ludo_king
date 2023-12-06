022a681674535bb455a20bcb8a4685c801f45090
tamrat-ake <tamratfekadu123@gmail.com>
2023-12-06 12:45:48 +0300

    creating switch player function

--- config.js
+++ config.js
@@ -15,6 +15,22 @@ const rondomDice = function () {
   return Math.floor(Math.random() * 6 + 1);
 };
 
+// creating switchPlayer function
+function switchPlayer() {
+  clearInterval(changeBgId);
+  clearInterval(animateArrowId);
+  document
+    .querySelector(`.dice-player-${activePlayer}`)
+    .classList.add("hidden");
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  animateArrow(activePlayer);
+
+  if (activePlayer === 1) changeOpacity(activePlayer, ["#2f9e44", "#59b169"]);
+  else changeOpacity(activePlayer, ["#00c8ff", "#4dd9ff"]);
+  document
+    .querySelector(`.dice-player-${activePlayer}`)
+    .classList.remove("hidden");
+}
 // creating buttons for both buttons
 
 btnEl0.addEventListener("click", function () {
