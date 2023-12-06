a5ef7bf5a8d4553f5ced7f9cde7b2d0bb0baaf2a
tamrat-ake <tamratfekadu123@gmail.com>
2023-12-06 12:39:12 +0300

    creating  the active player and initialization function

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 // selecting elements
 const btnEl0 = document.querySelector(".dice-player-0");
 const btnEl1 = document.querySelector(".dice-player-1");
+let activePlayer;
+function init() {
+  activePlayer = 0;
+
+  document
+    .querySelector(`.dice-player-${activePlayer}`)
+    .classList.remove("hidden");
+}
+init();
